f9a75792cdcbc252bdbb02b9ea62047a8f44e0db
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-03-08 13:17:29 -0400

    fix data

--- frontend/app/components/todo/TodoList.tsx
+++ frontend/app/components/todo/TodoList.tsx
@@ -11,23 +11,11 @@ const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
       <table className="table w-full">
         <thead>
           <tr>
-            <th>Tasks</th>
-            <th>Actions</th>
+            <th>Tareas</th>
+            <th>Acciones</th>
           </tr>
         </thead>
-        <tbody>
-          <td className="border px-4 py-2">TEST</td>
-          <td className="border px-4 py-2">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Edit
-            </button>
-          </td>
-          <td className="border px-4 py-2">
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-              Delete
-            </button>
-          </td>
-        </tbody>
+        <tbody></tbody>
       </table>
     </div>
   );
